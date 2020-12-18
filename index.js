ce1b250e3409a203505ae849b3da01c018cb6371
Merlin Rock <rock.merlin@yahoo.com>
2020-12-17 21:15:01 -0500

    Inquirer package installed

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const inquire = require("inquirer");
+
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What's the name of your project?"
+        
+    }
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
